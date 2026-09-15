33a54194a9f418b3f20bdb07bd63111ea1ac6d86
PRAYUKTH SHETTY <prayukth07@gmail.com>
2026-09-15 22:42:35 +0530

    fix: use aliased react-router-dom import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { useAuth } from './context/useAuth'
 import AppShell from './components/AppShell'
 import DashboardPage from './pages/DashboardPage'
